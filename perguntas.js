17028ad22bd1bfd0f5f33942566b80b3dc954c7e
ferlimaa <ferlimaed@gmail.com>
2024-10-10 08:47:47 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,119 @@
+criarCartao(
+'Química',
+'Qual é o elemento químico com o símbolo "Fe"?',
+'Ferro'
+)
+
+criarCartao(
+'História',
+'Em que ano a Segunda Guerra Mundial começou?',
+'1939'
+)
+
+criarCartao(
+'Geografia',
+'Quantos planetas existem no sistema solar?',
+'Oito'
+)
+
+criarCartao(
+'História',
+'Quem pintou a monalisa?',
+'Leoanardo da Vinci'
+)
+
+criarCartao(
+    'matemática',
+    'Quanto é 12+5?',
+    '17'
+)
+
+criarCartao(
+    'História',
+    'Qual o idioma mai falado no mundo?',
+    'Mandarim'
+)
+
+criarCartao(
+    'Geografia',
+    'Qual a capital o Brasil?',
+    'Brasília'
+)
+
+criarCartao(
+    'Literatura',
+    'Quem escreveu Romeu e Julieta?',
+    'William Shakespeare'
+)
+
+criarCartao(
+    'Biologia',
+    'Qual o maior animal terrestre',
+    'Elefante'
+)
+
+criarCartao(
+    'Artes',
+    'Quem pintou "A última ceia"?',
+    'Leonardo da Vinci'
+)
+
+criarCartao(
+    'História',
+    'Quando foi a indepêndencia do Brasil?',
+    '7 de setemnbro de 1822'
+)
+
+criarCartao(
+    'Biologia',
+    'Qual a função do coração no corpo humano?',
+    'Bombear o sangue'
+)
+
+criarCartao(
+    'História',
+    'Qual a data de indepêndencia dos Estados Unidos?',
+    '4 de julho de 1776'
+)
+
+criarCartao(
+'Matemática',
+'Quanto é 4+10',
+'14'
+)
+
+criarCartao(
+    'Geografia',
+    'Qual o maior deserto de mundo?',
+    'Deserto da Antártica'
+)
+
+criarCartao(
+    'Geografia',
+    'Qual o maior planeta do sistema solar?',
+    'Júpiter'
+)
+
+criarCartao(
+    'Química',
+    'Qual o gás responsável pelo efeito estufa?',
+    'Dióxido de carbono'
+)
+
+criarCartao(
+'Geografia', 
+'Qual a capital do paraná?',
+'Curitiba'
+)
+
+criarCartao(
+    'geografia',
+    'Quantos continentes existem?',
+    'sete'
+)
+
+criarCartao(
+    'astronomia',
+    'qual o planeta mais próximo do sol?',
+    'Mercúrio'
+)
